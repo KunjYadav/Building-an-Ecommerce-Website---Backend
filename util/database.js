39acae1518552646444b66adc75f44bba933c0c8
KunjYadav <kunj.yadav555@gmail.com>
2022-10-04 02:24:44 +0530

    Task -17 -> Backend ready to Deploy

--- util/database.js
+++ util/database.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('ecommerce', 'root', 'Kp@7000584374', {
+// console.log(process.env)
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.DB_HOST
 });
+
 
 module.exports = sequelize;
